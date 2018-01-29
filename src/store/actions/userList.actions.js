0460858ebdb35b2c748c5ae9a6b29f3e2136404a
Liubomyr Kuzenko <lubko.lintu@gmail.com>
2018-01-29 16:23:29 +0200

    Multiple API calls with Redux Thunks.

--- src/store/actions/userList.actions.js
+++ src/store/actions/userList.actions.js
@@ -2,14 +2,43 @@ import Http from '../../http.service'
 
 import { SAVE_USERS, ACTIVE_USER } from '../actions.const'
 
+function doSomething() {
+  return dispatch => 
+    fetch(
+      'https://swapi.co/api/people/'
+    ).then(
+      response => response.json()
+    )
+}
+
+function doSomethingElse() {
+  return dispatch => 
+    fetch(
+      'https://swapi.co/api/planets/3/'
+    ).then(
+      response => response.json()
+    )
+}
+
 // async action
 export const getUsers = () => dispatch => {
-  Http.get('https://swapi.co/api/people/').then(userlist => {
-    dispatch({
-      type: SAVE_USERS,
-      payload: userlist.results
-    })
-  })
+  // Http.get('https://swapi.co/api/people/').then(userlist => {
+  //   dispatch({
+  //     type: SAVE_USERS,
+  //     payload: userlist.results
+  //   })
+  // })
+
+  Promise.all([
+    dispatch(doSomething()),
+    dispatch(doSomethingElse())
+  ]).then((data) => {
+      console.log(data)
+      dispatch({
+        type: SAVE_USERS,
+        payload: data[0].results
+      })
+  });
 }
 
 // normal action
