2088b53a71c3698bcb9332ba2de29d69a51b576d
Liubomyr Kuzenko <lubko.lintu@gmail.com>
2018-02-19 15:18:15 +0200

    add enzyme support and some refactoring

--- src/__tests__/module.app.spec.js
+++ src/__tests__/module.app.spec.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom'
+import { shallow } from 'enzyme';
 import { App } from '../modules';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HashRouter><App/></HashRouter>, div);
+  shallow(<HashRouter><App/></HashRouter>);
 });
